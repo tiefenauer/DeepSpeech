704454531601bdb808bcaf276508ebbe383abd16
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-09-13 10:29:25 +0200

    working training on LDC93S1 use the following command to train again: ./bin/run-ldc93s1.sh

--- bin/run-ldc93s1.sh
+++ bin/run-ldc93s1.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -xe
+
+echo "Python directory:"
+which python3
+python3 --version
+
 if [ ! -f DeepSpeech.py ]; then
     echo "Please make sure you run this from DeepSpeech's top level directory."
     exit 1
@@ -7,7 +12,7 @@ fi;
 
 if [ ! -f "data/ldc93s1/ldc93s1.csv" ]; then
     echo "Downloading and preprocessing LDC93S1 example data, saving in ./data/ldc93s1."
-    python -u bin/import_ldc93s1.py ./data/ldc93s1
+    python3 -u bin/import_ldc93s1.py ./data/ldc93s1
 fi;
 
 if [ -d "${COMPUTE_KEEP_DIR}" ]; then
@@ -16,7 +21,7 @@ else
     checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/ldc93s1"))')
 fi
 
-python -u DeepSpeech.py \
+python3 -u DeepSpeech.py \
   --train_files data/ldc93s1/ldc93s1.csv \
   --dev_files data/ldc93s1/ldc93s1.csv \
   --test_files data/ldc93s1/ldc93s1.csv \
